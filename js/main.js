e6a808b83dbebbb437db99ff76c30b13043b4229
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-13 12:21:59 +0100

    Main js and getting values from form

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//
+document.getElementById('myform').addEventListener('submit', saveBookmark);
+
+//save bookmarks
+function saveBookmark(e){
+  // get form values
+  var siteName = document.getElementById('siteName');
+  var siteUrl = document.getElementById('siteUrl');
+
+  e.preventDefault();
+}
